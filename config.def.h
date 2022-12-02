eae0d3e86e48f26df3e6615bf7b67244a000ce64
Hoa Ho <hoviethoa@gmail.com>
2022-12-01 16:53:02 -0800

    Config font size for HiDPI screens

--- config.def.h
+++ config.def.h
@@ -5,10 +5,10 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "monospace:pixelsize=10:antialias=true:autohint=true";
+static char *font = "monospace:pixelsize=14:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
-	"NotoColorEmoji:pixelsize=16:antialias=true:autohint=true"
+	"NotoColorEmoji:pixelsize=26:antialias=true:autohint=true"
 /*	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true", */
 /*	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true", */
 };
